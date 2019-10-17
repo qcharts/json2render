63a798ded0362b385d217ad63427171167222a9e
yaotaiyang <yaotaiyang99@gmail.com>
2019-10-17 10:27:12 +0800

    dev some code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-export default function render(json) {
-  let obj = json
-  if (typeof json !== 'object') {
-    try {
-      obj = JSON.parse(json)
-    } catch (e) {
-      console.error(e)
-    }
+export default function json2render(json) {
+  let { container, qcharts, data, dataFields, graphics } = json
+  let { Chart } = qcharts
+  if (Chart) {
+    let chart = new Chart({
+      container: container
+    })
+    chart.source(data, dataFields)
+    graphics.forEach(graphic => {
+      let { type, attrs, styles } = graphic
+      if (qcharts[type]) {
+        let curGraphic = new qcharts[type](attrs)
+        if (styles) {
+          for (let key in styles) {
+            curGraphic.style(key, styles[key])
+          }
+        }
+        chart.add(curGraphic)
+      } else {
+        err('qcharts do not contains Class ' + type)
+      }
+    })
+    chart.render()
+    return chart
+  } else {
+    err('qcharts do not contains Class Chart')
   }
+}
+function err(msg) {
+  console.error(msg)
 }
